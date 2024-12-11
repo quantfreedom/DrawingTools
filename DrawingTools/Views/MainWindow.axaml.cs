31835deb1c7a85a7bf1056e177e58b0f7b94c3a3
Neo <rew9844@gmail.com>
2024-12-11 08:21:04 -0800

    init

---
+++ DrawingTools/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace DrawingTools.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
